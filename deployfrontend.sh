215b7ca0cbefc6bae7beddefd8d2e273c609af5c
Erik Raúl Vargas Bocanegra <erikraul@fb.com>
2022-02-14 20:23:00 +0000

    adding frontend files to Github pages

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -2,4 +2,5 @@ rsync -r src/ docs/
 rsync build/contracts/ChainList.json docs/
 git add .
 git commit -m "adding frontend files to Github pages"
+git remote add origin https://evcc2010.github.io/chainlist-truffle4/
 git push
